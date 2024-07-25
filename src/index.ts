d84a9d2808857e032e3337de8ebfbfafa69b2cbc
Jake Hensley <hensley.j@northeastern.edu>
2024-07-25 12:46:49 -0400

    fixed the typescript so it actually works

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 function sendText() {
-    const element = document.getElementById('text');
+    const element = document.getElementById("text");
     if (element){
-        element.textContent = 'Welcome to my website';
+        element.textContent = "Welcome to my website";
     }
 }
 sendText();
