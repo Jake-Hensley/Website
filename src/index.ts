7c51e139b9cef277170ccdf27b18df7266b1ff46
Jake Hensley <hensley.j@northeastern.edu>
2024-08-08 13:03:00 -0400

    Add navigation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { makeHomeView } from "./views/home.view.js";
+import { makeNavView } from "./views/navigation.view.js";
 import { renderView } from "./views/view.utils.js";
 
 function init() {
+    const navSection = document.getElementById("section-nav");
+    const navView = makeNavView();
+    navSection?.appendChild(navView);
+
     const homeView = makeHomeView();
     renderView(homeView);
 }
---
+++ src/views/navigation.view.ts
@@ -0,0 +1,34 @@
+import { makeAboutView } from "./about.view";
+import { makeHomeView } from "./home.view";
+import { renderView } from "./view.utils";
+
+type NavItem = [label: string, viewFn: Function];
+
+const NAV_ITEMS: NavItem[] = [
+    ["Home", makeHomeView],
+    ["About this site", makeAboutView]
+];
+
+export function makeNavView() {
+    const wrapper = document.createElement("div");
+
+    NAV_ITEMS.forEach((navItem) => {
+        const navLink = makeNavLink(...navItem);
+        wrapper.appendChild(navLink);
+    });
+    return wrapper;
+}
+export function makeNavLink(label: string, viewFn: Function) {
+    const link = document.createElement("a");
+    link.textContent = label;
+    link.addEventListener("click", handleNavClick(viewFn));
+    link.setAttribute("style", "padding: 2px; text-decoration: bold;");
+
+    return link;
+}
+export function handleNavClick(viewFn: Function) {
+    return function (event: MouseEvent) {
+        event.preventDefault();
+        renderView(viewFn());
+    };
+}
