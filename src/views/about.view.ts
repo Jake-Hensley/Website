fe800d9fc317769a826b73f940256578073711ae
Jake Hensley <hensley.j@northeastern.edu>
2024-08-07 22:18:19 -0400

    started to set stuff up NOT DONE

--- src/views/about.view.ts
+++ src/views/about.view.ts
@@ -1,12 +1,18 @@
-import { makeParagraph } from "./view.utils";
+import { makeLink, makeParagraph, makeSpan } from "./view.utils";
+
+const paragraph1 = [
+    makeSpan("This website is being developed using ");
+    makeLink("https://medium.com/swlh/refactoring-a-static-html-site-into-typescript-part-1-of-2-the-content-b4c975ecab00", "this"),
+    makeSpan(" article written by Prof. Simon Lutterbie."),
+];
 
 export function makeAboutView() {
-    //create a single element to contain the rest of the content
     const view = document.createElement('div');
-    //create content
-    const paragraph1 = makeParagraph('Test Words');
-    //add content to the view
-    view.appendChild(paragraph1)
+    const para1 = makeParagraph();
+    paragraph1.forEach((segment) => {para1.appendChild(segment);
 
+    }
+);
+    view.appendChild(para1);
     return view;
 }
--- src/views/view.utils.ts
+++ src/views/view.utils.ts
@@ -1,6 +1,6 @@
 
 
-export function makeParagraph(text: string) {
+export function makeParagraph(text = "") {
     const paragraph = document.createElement('p');
     paragraph.textContent = text;
     return paragraph;
@@ -25,16 +25,8 @@ export function makeSpan(text: string) {
 
 /* basic view creator
 export function makeAboutView() {
-
-    //create a single element to contain the rest of the content
     const view = document.createElement('div');
-
-    //create content
-    const paragraph1 = makeParagraph('Test Words');
-
-    //add content to the view
-    view.appendChild(paragraph1)
-
+    view.appendChild(...);
     return view;
 }
 */
