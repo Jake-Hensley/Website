8c556d4adf18b7482014888793b28f6bbe76b44a
Jake Hensley <hensley.j@northeastern.edu>
2024-08-08 17:33:15 -0400

    added author page

---
+++ src/views/author.view.ts
@@ -0,0 +1,9 @@
+import { makeLink, makeParagraph, makeSpan } from "./view.utils.js";
+
+const paragraph = makeParagraph("This website is currently being developed by Jake Hensley, a sophmore Computer Engineering Student at Northeatern University.");
+
+export function makeAuthorView() {
+    const view = document.createElement('div');
+    view.appendChild(paragraph);
+    return view;
+}
